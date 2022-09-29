8797b2b2cbcc1bd0b2c721d7b1905ceedfc9f30c
marceloAugustus42 <114123074+marceloAugustus42@users.noreply.github.com>
2022-09-29 10:27:39 -0300

    Create bd.js

---
+++ bd.js
@@ -0,0 +1,89 @@
+{
+  "areas" : [
+	{ 
+	  "id": 1,
+	  "nome": "Montanha do pinheiro",
+	  "avalicao" : 3.5,
+	  "idClass": 2,
+	  "tipoDeLocal": Aberto,
+	  "Latitude":-233.576.875,
+	  "Longitude":764.394.902
+	}, 
+	{ 
+	  "id": 2,
+	  "nome": "Riacho da Paz",
+	  "avaliacao" : 5,
+	  "idClass": 5,
+	  "tipoDeLocal": Aberto,
+	  "Latitude":783.326.221,
+	  "Longitude":123.908.324
+	}, 
+	{ 
+	  "id": 3,
+	  "nome": "Facão",
+	  "avaliacao" : 5,
+	  "idClass": 2,
+	  "tipoDeLocal": Fechado,
+	  "Latitude":740.002.754,
+	  "Longitude":-472.994.122
+	} 
+  ],
+  "classes": [
+  	{
+	 "id": 1,
+	 "tipo": "Plana"
+	},
+	{
+	 "id": 2,
+	 "tipo": "Montanhosa"
+	},
+	{
+	 "id": 3,
+	 "tipo": "Beira de rio"
+	},
+	{
+	 "id": 4,
+	 "tipo": "Desertica"
+	},
+	{
+	 "id": 5,
+	 "tipo": "Beira de riacho"
+	}
+  ],
+  "comentarios": [
+    {
+     "id": 1,
+     "body": "venta muito",
+	 "avalicao": 3
+     "areaId": 1,
+	 "userId": 322
+    },
+	{
+     "id": 2,
+     "body": "bem gelado",
+	 "avalicao": 4
+     "areaId": 1,
+	 "userId": 654
+    },
+	{
+     "id": 3,
+     "body": "muito tranquilo",
+	 "avalicao": 5
+     "areaId": 2
+	 "userId": 93
+    },
+	{
+     "id": 4,
+     "body": "bom para pescar"
+     "areaId": 2,
+	 "userId": 1
+    },
+	{
+     "id": 5,
+     "body": "dificil de chegar"
+	 "avalicao": 5
+     "areaId": 3,
+	 "userId": 837
+    }
+  ]
+}
